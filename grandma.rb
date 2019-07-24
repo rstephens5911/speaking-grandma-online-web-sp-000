1fc52753a88d6dad7945623d2dc76baa1744e686
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-24 22:55:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  shout = phrase.uppercase
+  # shout = phrase.uppercase
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase
