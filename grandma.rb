d0f96f6d86fb3d6510a105d9b8ceab640b83e018
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-24 22:57:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif shout == phrase
+  elsif shout
     return "NO, NOT SINCE 1938!"
   end
 end
