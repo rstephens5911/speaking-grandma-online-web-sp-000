0692345b082d733d2e0d1fb3e39798bbe0548059
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-24 22:49:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == !phrase.uppercase
+  elsif phrase == phrase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.uppercase
     return "NO, NOT SINCE 1938!"
