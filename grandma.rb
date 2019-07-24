fd78649e702f63d4501222c3d1d346ba6771de36
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-24 22:47:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,15 +7,15 @@ def speak_to_grandma(phrase)
   elsif phrase.uppercase
     return "NO, NOT SINCE 1938!"
   end
-  
-    
-    
+
+
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
